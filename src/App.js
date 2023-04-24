67ef6f6fc34db9feaaf8232f34998ab093e477d7
Rushit Patel <rushitpatel163@gmail.com>
2023-04-24 11:30:16 +0530

    Weather Card Created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Search from "./components/search";
+import Weather from "./components/weather";
 
 function App() {
   const handleOnSearchChange = (searchData)=>{
       console.log(searchData)
   }
     return (
-    <div style={{backgroundColor : '#e5e5e5'}} className="min-h-[100vh]">
-      <div className="flex flex-col items-center justify-center ">
+    <div className="bg-gradient-to-br from-pink-200 via-purple-200 to-indigo-200">
+      <div className="flex flex-col items-center justify-center pt-3">
       <Search onSearchChange={handleOnSearchChange}/>
       </div>
-      
+      <Weather/>      
     </div>
   );
 }
